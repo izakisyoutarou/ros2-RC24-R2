83959f0d73a21892cb7a6b3166890fbcee02e8fa
KazumaFujino <c1000702@planet.kanazawa-it.ac.jp>
2022-10-19 01:37:36 +0900

    ✨feat:mcl 2d

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -1,6 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rcl/rcl.h>
 #include "socketcan_interface/socketcan_interface_node.hpp"
+#include "mcl_2d/mcl_2d_node.hpp"
 #include <iostream>
 
 int main(int argc, char * argv[]){
@@ -8,7 +9,10 @@ int main(int argc, char * argv[]){
     rclcpp::executors::MultiThreadedExecutor exec;
 
     auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>();
-    exec.add_node(socketcan_node);
+    auto mcl_2d_node = std::make_shared<mcl_2d::Mcl2D>();
+
+    // exec.add_node(socketcan_node);
+    exec.add_node(mcl_2d_node);
 
     exec.spin();
     rclcpp::shutdown();
