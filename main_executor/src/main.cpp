7e84428041b0338366cc5ef021d4c7851dcda696
KazumaFujino <c1000702@planet.kanazawa-it.ac.jp>
2023-02-17 12:11:00 +0900

    ✨feat:spline pid

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -2,6 +2,7 @@
 #include <rcl/rcl.h>
 #include "socketcan_interface/socketcan_interface_node.hpp"
 #include "mcl_2d/mcl_2d_node.hpp"
+#include "spline_pid/spline_pid_node.hpp"
 #include <iostream>
 
 int main(int argc, char * argv[]){
@@ -12,11 +13,13 @@ int main(int argc, char * argv[]){
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
-    auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
+    // auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     auto mcl_2d_node = std::make_shared<mcl_2d::Mcl2D>(nodes_option);
+    auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
 
-    exec.add_node(socketcan_node);
+    // exec.add_node(socketcan_node);
     exec.add_node(mcl_2d_node);
+    exec.add_node(spline_pid_node);
 
     exec.spin();
     rclcpp::shutdown();
