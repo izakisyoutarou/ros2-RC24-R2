870591f3b18ea76da189b2902baa95b00d3721ef
ryosei1250 <c1103441@planet.kanazawa-it.ac.jp>
2023-10-01 14:01:29 +0900

    first commit

---
+++ main_executor/src/main.cpp
@@ -0,0 +1,21 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rcl/rcl.h>
+#include "socketcan_interface/socketcan_interface_node.hpp"
+#include <iostream>
+
+int main(int argc, char * argv[]){
+    rclcpp::init(argc,argv);
+    rclcpp::executors::MultiThreadedExecutor exec;
+
+    rclcpp::NodeOptions nodes_option;
+    nodes_option.allow_undeclared_parameters(true);
+    nodes_option.automatically_declare_parameters_from_overrides(true);
+
+    // auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
+
+    // exec.add_node(socketcan_node);
+
+    exec.spin();
+    rclcpp::shutdown();
+    return 0;
+}
