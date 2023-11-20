48fb2acc8f92df92f745444b0a8ba0ccbbef385d
Yamasaki-Daiki <c1206157@st.kanazawa-it.ac.jp>
2023-11-20 19:58:15 +0900

    change:main_executor

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -3,11 +3,6 @@
 #include "socketcan_interface/socketcan_interface_node.hpp"
 #include "spline_pid/spline_pid_node.hpp"
 #include "ransac_localization/ransac_localization.hpp"
-#include "controller_interface/controller_interface_node.hpp"
-#include "injection_interface/injection_interface_node.hpp"
-#include "ransac_localization/ransac_localization.hpp"
-#include "sequencer/sequencer_node.hpp"
-#include "injection_param_calculator/injection_param_calculator.hpp"
 #include <iostream>
 
 int main(int argc, char * argv[]){
@@ -18,21 +13,13 @@ int main(int argc, char * argv[]){
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
-    auto sequencer_node = std::make_shared<sequencer::Sequencer>(nodes_option);
-    auto controller_node = std::make_shared<controller_interface::SmartphoneGamepad>(nodes_option);
-    auto injection_interface_node = std::make_shared<injection_interface::InjectionInterface>(nodes_option);
     auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     auto ransac_localization = std::make_shared<self_localization::ransaclocalization>(nodes_option);
     auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
-    auto injection_param_calculator_node = std::make_shared<injection_param_calculator::InjectionParamCalculator>(nodes_option);
 
-    exec.add_node(sequencer_node);
-    exec.add_node(controller_node);
-    exec.add_node(injection_interface_node);
     exec.add_node(socketcan_node);
     exec.add_node(ransac_localization);
     exec.add_node(spline_pid_node);   
-    exec.add_node(injection_param_calculator_node);
 
     exec.spin();
     rclcpp::shutdown();
